6e2673f73b58636c6c96b532f5b465bc681fc15a
JC <jc@lifove.net>
2018-05-19 19:48:26 +0900

    Implement CLI

--- src/main/java/edu/handong/csee/java/examples/Runner.java
+++ src/main/java/edu/handong/csee/java/examples/Runner.java
@@ -1,9 +1,90 @@
 package edu.handong.csee.java.examples;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
 public class Runner {
+	
+	String path;
+	boolean verbose;
+	boolean help;
 
 	public static void main(String[] args) {
 
+		Runner myRunner = new Runner();
+		myRunner.run(args);
+
+	}
+
+	private void run(String[] args) {
+		Options options = createOptions();
+		
+		if(parseOptions(options, args)){
+			if (help){
+				printHelp(options);
+				return;
+			}
+		}
+
+	}
+
+	private boolean parseOptions(Options options, String[] args) {
+		CommandLineParser parser = new DefaultParser();
+
+		try {
+
+			CommandLine cmd = parser.parse(options, args);
+
+			path = cmd.getOptionValue("p");
+			verbose = cmd.hasOption("v");
+			help = cmd.hasOption("h");
+
+		} catch (Exception e) {
+			printHelp(options);
+			return false;
+		}
+
+		return true;
+	}
+
+	// Definition Stage
+	private Options createOptions() {
+		Options options = new Options();
+
+		// add options by using OptionBuilder
+		options.addOption(Option.builder("p").longOpt("path")
+				.desc("Set a path of a directory or a file to display")
+				.hasArg()
+				.argName("Path name to display")
+				.required()
+				.build());
+
+		// add options by using OptionBuilder
+		options.addOption(Option.builder("v").longOpt("verbose")
+				.desc("Display detailed messages!")
+				.hasArg()
+				.argName("verbose option")
+				//.required() // this is an optional option. So disabled required().
+				.build());
+		
+		// add options by using OptionBuilder
+		options.addOption(Option.builder("h").longOpt("help")
+		        .desc("Help")
+		        .build());
+
+		return options;
+	}
+	
+	private void printHelp(Options options) {
+		// automatically generate the help statement
+		HelpFormatter formatter = new HelpFormatter();
+		String header = "CLI test program";
+		String footer ="\nPlease report issues at https://github.com/lifove/CLIExample/issues";
+		formatter.printHelp("./CLIExample", header, options, footer, true);
 	}
 
 }
